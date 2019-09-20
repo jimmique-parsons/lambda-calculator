eadad8c16716379d81718db62262ab532fef3922
Jimmique <jimmique88@gmail.com>
2019-09-20 15:06:37 -0700

    Imported array data and set state for data sets

--- src/components/ButtonComponents/SpecialButtons/Specials.js
+++ src/components/ButtonComponents/SpecialButtons/Specials.js
@@ -1,11 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 
 //import any components needed
 
 //Import your array data to from the provided data file
+import { specials } from '../../../data';
 
 const Specials = () => {
   // STEP 2 - add the imported data to state
+  const [specialState, setSpecialState] = useState(specials);
 
   return (
     <div>
