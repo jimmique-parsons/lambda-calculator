14eb88daa3b3c00202807eb73e25c894f31d0f3b
Jimmique <jimmique88@gmail.com>
2019-09-20 15:14:56 -0700

    Passed and mapped data for Numbers

--- src/components/ButtonComponents/NumberButtons/Numbers.js
+++ src/components/ButtonComponents/NumberButtons/Numbers.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import NumberButton from "./NumberButton.js";
 
 //import any components needed
 // example of import from data.js. Note all the ../   This is how we move through folders. 
@@ -16,6 +17,7 @@ const [numberState, setNumberState] = useState(numbers);
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
        it any props needed by the child component*/}
+       { numbers.map(number => <NumberButton number={number}/>) }
     </div>
   );
 };
